07b8479669be674ca0b6d806f72dd57b855f09de
kapacenaaroslav <kapacenaaroslav@gmail.com>
2023-05-07 19:17:56 +0300

    I finished all program.

--- src/com/example/Forest/Forest.java
+++ src/com/example/Forest/Forest.java
@@ -1,43 +1,61 @@
 package com.example.Forest;
 
+import com.example.Habitat.Habitat;
+
 import java.util.ArrayList;
 
-public class Forest<T> {
+public class Forest<T> implements Habitat {
     private ArrayList<T> forest = new ArrayList<>();
-    private  String treeName;
     public Forest(){
     }
-    public Forest(ArrayList<T> forest,String treeName) {
-        this.forest = forest;
+
+    public Forest(ArrayList<T> forest) {
         this.forest = forest;
     }
+
     public ArrayList<T> getForest() {
         return forest;
     }
+
     public void setForest(ArrayList<T> forest) {
         this.forest = forest;
     }
-    public String getTreeName() {
-        return treeName;
-    }
-    public void setTreeName(String treeName) {
-        this.treeName = treeName;
-    }
-
-    public void forestAdd(T tree){
+    public void add(T tree){
         forest.add(tree);
     }
-    public void forestDelete(T tree){
+    public void delete(T tree){
         forest.remove(tree);
     }
-    public void getTree(Forest forests){
-        for (int i = 0; i < forest.size();i++){
-            System.out.println("Дерево в лесу: "+forest.get(i));
+    public void  allTrees( Forest forests){
+        for(int i = 0;i<forest.size();i++){
+            forest.get(i);
+        }
+    }
+    public void numberOfTrees(Forest forests){
+        forest.size();
+        System.out.println(forest.size());
+    }
+
+    @Override
+    public void animalAdd(T animal) {
+        forest.add(animal);
+    }
+
+    @Override
+    public void animalDelete(T animal) {
+        forest.remove(animal);
+    }
+
+    @Override
+    public void getAllAnimals(Forest forests) {
+        for(int i =0;i< forest.size();i++){
+            forest.get(i);
         }
     }
 
     @Override
-    public String toString() {
-        return forest+treeName;
+    public void numberOfAnimals(Forest forests) {
+        forest.size();
+        System.out.println(forest.size());
     }
 }
--- src/com/example/Habitat/Habitat.java
+++ src/com/example/Habitat/Habitat.java
@@ -2,7 +2,12 @@ package com.example.Habitat;
 
 import com.example.Forest.Forest;
 
-public interface Habitat {
+import java.util.ArrayList;
 
+public interface Habitat<T> {
+   void animalAdd(T animal);
+   void animalDelete(T animal);
+   void getAllAnimals(Forest forests);
+   void numberOfAnimals(Forest forests);
 
 }
